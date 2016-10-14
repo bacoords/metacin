794a977f8e20d1dee5befb38a466b43d84c80d41
bacoords <bacoords@gmail.com>
2016-10-14 13:39:17 -0700

    adds gallery

--- src/js/vue-app.js
+++ src/js/vue-app.js
@@ -10,53 +10,21 @@ var Home = Vue.extend({
   },
   methods : {
     playAudio : function() {
+      $('.mpcard--music').addClass('hover');
       if(!this.playing){
         $('.mpaudio__player').get(0).play();  
-        $('.mpcard__back > i').removeClass('fa-play-circle').addClass('fa-pause');
+        $('.mpcard__back__inner > i').removeClass('fa-play-circle').addClass('fa-pause');
       } else {
         $('.mpaudio__player').get(0).pause();
-        $('.mpcard__back > i').removeClass('fa-pause').addClass('fa-play-circle');
+        $('.mpcard__back__inner > i').removeClass('fa-pause').addClass('fa-play-circle');
       }
       this.playing = !this.playing;
       return;
-    },
+    }
   },
   mounted : function(){
     this.$nextTick(function (){
   
-      $('.main-entry').addClass('main-entry--return-home');
-      $('.main-entry').removeClass('main-entry--open-all main-entry--open-featured main-entry--open-music main-entry--open-gallery main-entry--open-laboratory'); 
-
-      //Circle Hover Stuff
-
-      $('#circle-bottom-bg').on('mouseenter',function(){
-        $('.logo').removeClass('circle-top-run circle-right-run circle-left-run').width();
-        $('.logo').addClass('circle-bottom-run');
-      });
-
-      $('#circle-left-bg').on('mouseenter',function(){
-        $('.logo').removeClass('circle-top-run circle-right-run circle-bottom-run').width();
-        $('.logo').addClass('circle-left-run');
-      });
-
-      $('#circle-right-bg').on('mouseenter',function(){
-        $('.logo').removeClass('circle-top-run circle-bottom-run circle-left-run').width();
-        $('.logo').addClass('circle-right-run');
-      });
-
-      $('#circle-top-bg').on('mouseenter',function(){
-        $('.logo').removeClass('circle-right-run circle-bottom-run circle-left-run').width();
-        $('.logo').addClass('circle-top-run');
-      });
-
-      $('.feat-featured').click(function(){
-        var aTag = $('.router-contents');
-        $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-      });
-
-      $('.circle-left-run svg').click(function(){
-        router.push({path : '/music'});
-      });
 
       var self = this;
       $('.header__menu-item').click(function(){
@@ -78,8 +46,7 @@ var Home = Vue.extend({
 
 
       $('html, body').scrollTop(0);
-      //Rellax Init
-      var rellax = new Rellax('.rellax');
+
 
 
 
@@ -222,12 +189,13 @@ var Music = Vue.extend({
       return;
     },
     playAudio : function() {
+      $('.mpcard--music').addClass('hover');
       if(!this.playing){
         $('.mpaudio__player').get(0).play();  
-        $('.mpcard__back > i').removeClass('fa-play-circle').addClass('fa-pause');
+        $('.mpcard__back__inner > i').removeClass('fa-play-circle').addClass('fa-pause');
       } else {
         $('.mpaudio__player').get(0).pause();
-        $('.mpcard__back > i').removeClass('fa-pause').addClass('fa-play-circle');
+        $('.mpcard__back__inner > i').removeClass('fa-pause').addClass('fa-play-circle');
       }
       this.playing = !this.playing;
       return;
@@ -268,12 +236,7 @@ var Music = Vue.extend({
       this.preload(a, function(){
         self.loaded = true;
         document.body.style.overflow = 'auto';
-        $('.main-entry').addClass('main-entry--return-home');
-        $('.main-entry').removeClass('main-entry--open-all main-entry--open-featured main-entry--open-music main-entry--open-gallery main-entry--open-laboratory');
-        $('.main-entry').removeClass('main-entry--return-home');
-        $('.main-entry').addClass('main-entry--open-all main-entry--open-music');
-        $('.logo').removeClass('circle-right-run circle-left-run circle-bottom-run circle-top-run').width();
-
+ 
         
 
         $('.mpgal__item img').hover(function(){
@@ -281,9 +244,11 @@ var Music = Vue.extend({
         }, function(){
           $('.mpgal').removeClass('mpgal--hovered');
         });
+
+        //SCROLLTOP
         $('html, body').scrollTop(0);
-        //Rellax Init
-        var rellax = new Rellax('.rellax'); 
+        
+   
 
         //Appear on animation
         var $animation_elements = $('.mpfadein');
@@ -314,43 +279,160 @@ var Music = Vue.extend({
         $window.trigger('scroll');
 
       });
+      
+      
+      
     });
     
   }
 });
  
-
 var Gallery = Vue.extend({
-  template : '<h1>Gallery</h1>',
+  template : '#galleryTemplate',
   data: function(){
     return {
-      loaded : false
+      loaded : false,
+      posts : [
+        {
+          title : 'Earth',
+          slug : 'earth',
+          image : '/dist/img/gallery-1.png',
+          content : '<p>When I get off of this mountain You know where I want to go Straight down the Mississippi River To the Gulf of Mexico </p><p> To Lake George, Louisiana Little Bessie, girl that I once knew And she told me just to come on by If there\'s anything she could do </p><p> Up on Cripple Creek she sends me If I spring a leak she mends me I don\'t have to speak she defends me A drunkard\'s dream if I ever did see one</p>'
+        },
+        {
+          title : 'Sirens',
+          slug : 'sirens',
+          image : '/dist/img/gallery-2.png',
+          content : '<p>When I get off of this mountain You know where I want to go Straight down the Mississippi River To the Gulf of Mexico </p><p> To Lake George, Louisiana Little Bessie, girl that I once knew And she told me just to come on by If there\'s anything she could do </p><p> Up on Cripple Creek she sends me If I spring a leak she mends me I don\'t have to speak she defends me A drunkard\'s dream if I ever did see one</p>'
+        },
+        {
+          title : 'Autumn',
+          slug : 'autumn',
+          image : '/dist/img/gallery-3.png',
+          content : '<p>When I get off of this mountain You know where I want to go Straight down the Mississippi River To the Gulf of Mexico </p><p> To Lake George, Louisiana Little Bessie, girl that I once knew And she told me just to come on by If there\'s anything she could do </p><p> Up on Cripple Creek she sends me If I spring a leak she mends me I don\'t have to speak she defends me A drunkard\'s dream if I ever did see one</p>'
+        },
+        {
+          title : 'Sky',
+          slug : 'sky',
+          image : '/dist/img/gallery-4.png',
+          content : '<p>When I get off of this mountain You know where I want to go Straight down the Mississippi River To the Gulf of Mexico </p><p> To Lake George, Louisiana Little Bessie, girl that I once knew And she told me just to come on by If there\'s anything she could do </p><p> Up on Cripple Creek she sends me If I spring a leak she mends me I don\'t have to speak she defends me A drunkard\'s dream if I ever did see one</p>'
+        },
+        {
+          title : 'Winter',
+          slug : 'winter',
+          image : '/dist/img/gallery-5.png',
+          content : '<p>When I get off of this mountain You know where I want to go Straight down the Mississippi River To the Gulf of Mexico </p><p> To Lake George, Louisiana Little Bessie, girl that I once knew And she told me just to come on by If there\'s anything she could do </p><p> Up on Cripple Creek she sends me If I spring a leak she mends me I don\'t have to speak she defends me A drunkard\'s dream if I ever did see one</p>'
+        },
+        {
+          title : 'Solstice',
+          slug : 'solstice',
+          image : '/dist/img/gallery-6.png',
+          content : '<p>When I get off of this mountain You know where I want to go Straight down the Mississippi River To the Gulf of Mexico </p><p> To Lake George, Louisiana Little Bessie, girl that I once knew And she told me just to come on by If there\'s anything she could do </p><p> Up on Cripple Creek she sends me If I spring a leak she mends me I don\'t have to speak she defends me A drunkard\'s dream if I ever did see one</p>'
+        },
+        {
+          title : 'Obelisk',
+          slug : 'obelisk',
+          image : '/dist/img/gallery-7.png',
+          content : '<p>When I get off of this mountain You know where I want to go Straight down the Mississippi River To the Gulf of Mexico </p><p> To Lake George, Louisiana Little Bessie, girl that I once knew And she told me just to come on by If there\'s anything she could do </p><p> Up on Cripple Creek she sends me If I spring a leak she mends me I don\'t have to speak she defends me A drunkard\'s dream if I ever did see one</p>'
+        },
+        {
+          title : 'Canon in D',
+          slug : 'canon-in-d',
+          image : '/dist/img/gallery-8.png',
+          content : '<p>When I get off of this mountain You know where I want to go Straight down the Mississippi River To the Gulf of Mexico </p><p> To Lake George, Louisiana Little Bessie, girl that I once knew And she told me just to come on by If there\'s anything she could do </p><p> Up on Cripple Creek she sends me If I spring a leak she mends me I don\'t have to speak she defends me A drunkard\'s dream if I ever did see one</p>'
+        }
+      ]
+    }
+  },
+  methods : {
+    preload : function(pictureUrls, callback) {
+        var i,
+            j,
+            loaded = 0;
+
+        for (i = 0, j = pictureUrls.length; i < j; i++) {
+            (function (img, src) {
+                img.onload = function () {                               
+                    if (++loaded == pictureUrls.length && callback) {
+                        callback();
+                    }
+                };
+
+                // Use the following callback methods to debug
+                // in case of an unexpected behavior.
+                img.onerror = function () {};
+                img.onabort = function () {};
+
+                img.src = src;
+            } (new Image(), pictureUrls[i]));
+        }
     }
   },
   mounted : function(){
     this.$nextTick(function(){
-      
-      $('.main-entry').addClass('main-entry--return-home');
-      $('.main-entry').removeClass('main-entry--open-all main-entry--open-featured main-entry--open-music main-entry--open-gallery main-entry--open-laboratory');
 
+      
 
-      $('.main-entry').removeClass('main-entry--return-home');
-      $('.main-entry').addClass('main-entry--open-all main-entry--open-gallery');
-      $('.logo').removeClass('circle-right-run circle-left-run circle-bottom-run circle-top-run').width();
+      document.body.style.overflow = 'hidden';
+      var a = ['/dist/img/icon-amazon.png','/dist/img/icon-spotify.png','/dist/img/icon-itunes.png']; 
+      this.posts.forEach(function(p){
+        a.push(p.image);
+      });
+      var self = this;
+      this.preload(a, function(){
+        self.loaded = true;
+        document.body.style.overflow = 'auto';
+ 
+        
+
+        $('.mpgal__item img').hover(function(){
+          $('.mpgal').addClass('mpgal--hovered');
+        }, function(){
+          $('.mpgal').removeClass('mpgal--hovered');
+        });
+
+        //SCROLLTOP
+        $('html, body').scrollTop(0);
+        
+   
+
+        //Appear on animation
+        var $animation_elements = $('.mpfadein');
+        var $window = $(window);
+
+        function check_if_in_view() {
+          var window_height = $window.height();
+          var window_top_position = ($window.scrollTop() + 150);
+          var window_bottom_position = (window_top_position + window_height - 150);
+
+          $.each($animation_elements, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+              (element_top_position <= window_bottom_position)) {
+              $element.addClass('mpfadein--active');
+            } else {
+              $element.removeClass('mpfadein--active');
+            }
+          });
+        }
+
+        $window.on('scroll resize', check_if_in_view);
+        $window.trigger('scroll');
+
+      });
       
-      
-      $('html, body').scrollTop(0);
-      //Rellax Init
-      var rellax = new Rellax('.rellax');      
-      this.loaded = true;
 
     });
   }
 });
  
-
 var Laboratory = Vue.extend({
-  template : '<h1>Laboratory</h1>',
+  template : '<div class="coming-soon"><h1>Laboratory</h1><h2>Coming Soon</h2></div>',
   data: function(){
     return {
       loaded : false 
@@ -359,19 +441,24 @@ var Laboratory = Vue.extend({
   mounted : function(){
     this.$nextTick(function(){
 
-      $('.main-entry').addClass('main-entry--return-home');
-      $('.main-entry').removeClass('main-entry--open-all main-entry--open-featured main-entry--open-music main-entry--open-gallery main-entry--open-laboratory');
-
-
-      $('.main-entry').removeClass('main-entry--return-home');
-      $('.main-entry').addClass('main-entry--open-all main-entry--open-laboratory');
-      $('.logo').removeClass('circle-right-run circle-left-run circle-bottom-run circle-top-run').width();
-        
-      
       $('html, body').scrollTop(0);
-      //Rellax Init
-      var rellax = new Rellax('.rellax'); 
-      
+
+    });
+  }
+});
+
+var Events = Vue.extend({
+  template : '<div class="coming-soon"><h1>Events</h1><h2>Coming Soon</h2></div>',
+  data: function(){
+    return {
+      loaded : false 
+    }
+  },
+  mounted : function(){
+    this.$nextTick(function(){
+
+      $('html, body').scrollTop(0);
+ 
     });
   }
 });
@@ -431,6 +518,60 @@ var Album = Vue.extend({
   }
 });
 
+var Art = Vue.extend({
+  template : '#artTemplate',
+  data : function(){
+    return {
+      post : {
+        title : '',
+        content : '',
+        image : '',
+        slug : ''
+      }
+    }
+  },
+  beforeDestroy : function(){
+    $('.mpmodal').removeClass('mpmodal--active');
+    document.body.style.overflow = 'auto';
+    
+  },
+  methods : {
+    closeAlbum : function(){
+      router.push({path : '/gallery'});
+      return;
+    }
+  },
+  mounted : function(){
+    this.$nextTick(function(){
+      
+      var self = this;
+      var p = this.$parent.posts.filter(function(post){
+        return post.slug == self.$route.params.slug;
+      });
+      console.log(p[0]);
+      this.post = Object.assign({}, { content : p[0].content, image : p[0].image, slug : p[0].slug, title : p[0].title });
+      console.log(this.post);
+
+
+      //Display Properties
+      $('.mpmodal').addClass('mpmodal--active');
+      document.body.style.overflow = 'hidden';
+
+      //Back on Escape
+      $(document).keyup(function(e) {
+        if (e.keyCode == 27) { // escape key maps to keycode `27`
+          router.push({path : '/gallery'});
+        }
+      });
+      
+      $('.mpmodal__exit').click(function(){
+        console.log('exit');  
+        self.closeAlbum();
+      });
+      
+    });
+  }
+});
 
 // Create a router instance.
 // You can pass in additional options here, but let's
@@ -440,14 +581,17 @@ var router = new VueRouter({
   routes : [
     {
       path : '/',
+      name : 'home',
       component: Home
     },
     {
-      path : '/index.html',  
+      path : '/index.html', 
+      name : 'home', 
       component: Home
     },
     {
       path : '/music',
+      name : 'music', 
       component : Music,
       children : [
         {
@@ -458,11 +602,28 @@ var router = new VueRouter({
     },
     {
       path : '/gallery',
-      component: Gallery
+      name : 'gallery', 
+      component: Gallery,
+      children : [
+        {
+          path : ':slug',
+          component : Art
+        }
+      ]
     },
     {
       path : '/laboratory',
+      name : 'laboratory', 
       component: Laboratory
+    },
+    {
+      path : '/events',
+      name : 'events', 
+      component: Events
+    },
+    {
+      path : '*',
+      redirect : '/'
     }
   ]
 });
